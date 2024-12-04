abe210be176b22cc77b25b53b0fc2cff127f50fa
maciekdt <74464525+maciekdt@users.noreply.github.com>
2024-12-04 22:05:38 +0100

    ..

--- video_create.py
+++ video_create.py
@@ -1,12 +1,11 @@
 from src.video_creator.video_creator import VideoCreator
-from src.model.DQN_model import dqn_model
 import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 video_creator = VideoCreator(
-    model_path="saved_models/dqn_snake_40M.zip",
+    model_path="logs/saved_models/best_checkpoints/best_model.zip",
     video_path="videos/snake_game_video.mp4",
     fps=20
 )
