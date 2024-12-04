bccdd30b6c22b043268f9325b3f5c19296a6e5e9
maciekdt <74464525+maciekdt@users.noreply.github.com>
2024-12-04 22:11:32 +0100

    ...

--- video_create.py
+++ video_create.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 video_creator = VideoCreator(
     model_path="logs/saved_models/best_checkpoints/best_model.zip",
-    video_path="videos/snake_game_video.mp4",
+    video_path="logs/log_videos/snake_game_video.mp4",
     fps=20
 )
 video_creator.create_video(num_steps=500)
